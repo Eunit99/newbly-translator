cb5cc83cd01966c3acd0a6d1bd78a01d8dc6c6dd
Ξunit <eunithelp@gmail.com>
2022-09-21 17:33:43 +0100

    Feat: Preliminary add of script to fetch data from the backend API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,119 +1,156 @@
 $(document).ready(function ($) {
 
-  // Get all all classes with .newbly in the document
-  $.fn.newbly = function () {
-    var newblyClass = $(".newbly");
+  var fetchArticleFromBackend = function () {
+    var newblyBackendAPI = "https://api.newb.ly/articles/gVe8WHhm?language=english&country=austria&fbclid=IwAR3IA_dgK8W_kakCh44PUJv3lMajeJWYqIotGcSdlSMFnFRKGS3yeceZp3o";
+
+    $.ajax({
+      url: newblyBackendAPI,
+      dataType: "json",
+      success: function (result) {
+        fetchArticleCategory(result);
+      },
+      error: function (xhr) {
+        console.error("An error occurred: " + xhr.status + " " + xhr.statusText);
+      }
+    });
+  }
+
+  // Set the fetch data
+  var fetchArticleCategory = function (data) {
+    fetchArticleTitle(data.articleTitle);
+    fetchArticleTitleTranslated(data.articleTitleTranslated);
+    fetchArticleContent(data.articleContent);
+    fetchArticleContentTranslated(data.articleContentTranslated);
+
+    return data;
+
+  }
 
 
 
-    // Grab all classes with .newbly in the document
-    var getText = function () {
-      newblyClass.map(function () {
 
-        return this.innerText;
 
-      }).get();
+
+
+
+
+
+  var fetchArticleTitle = function (data) {
+    console.log(data);
+    searchIfTextExist(data);
+
+    return data;
+  }
+
+  var fetchArticleTitleTranslated = function (data) {
+    console.log(data);
+    return data;
+  }
+
+
+  var fetchArticleContent = function (data) {
+    console.log(data);
+
+    // TODO Loop the array of data and call searchIfTextExist(data)
+
+
+    searchIfTextExist(data);
+
+    return data;
+  }
+
+  var fetchArticleContentTranslated = function (data) {
+    console.log(data);
+
+    // TODO Loop the array of data and call replaceText(data) ?
+
+    return data;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  var searchIfTextExist = function (data) {
+
+    if (document.body.textContent.includes(data)) {
+      appendText(data);
+    } else {
+      console.error('text does not exist on page');
     }
 
+  }
 
-    var replaceText = function () {
-      newblyClass.each(function () {
 
-        var text = $(this);
+  var appendText = function (data) {
+    // TODO FIND THE PARENT where data is coming from and append it to the documentElement
+    document.innerText = data;
+  }
 
 
 
-        $.ajax({
-          url: 'https://randomuser.me/api/',
-          dataType: 'json',
-          success: function (data) {
 
-            var randomUser = data;
-            console.log(text.text());
 
-            // Temporarily use the text from random users and replace texts
-            text.append("<br /> Random user: " + randomUser.results[0].name.title + " " + randomUser.results[0].name.first + " " + randomUser.results[0].name.last + "<br /> <br />");
 
-          }
-        });
 
 
-      });
 
-    }
+  // fetchArticleFromBackend();
 
-    replaceText();
-    // console.log(getText)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  };
-
-
-
-
-  // TODO Write a code to translate the contents
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // call the newbly function
-  $("div").newbly()
 }(jQuery));
+
+
