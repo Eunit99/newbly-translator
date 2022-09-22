0d83985cdc56c52f88bec9c125effcfc3190f01d
Ξunit <eunithelp@gmail.com>
2022-09-22 11:42:20 +0100

    Feat:  Set up appending translated articleTitle

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,10 +18,13 @@ $(document).ready(function ($) {
   // Set the fetch data
   var fetchArticleCategory = function (data) {
     fetchArticleTitle(data.articleTitle);
-    fetchArticleTitleTranslated(data.articleTitleTranslated);
     fetchArticleContent(data.articleContent);
     fetchArticleContentTranslated(data.articleContentTranslated);
 
+
+
+
+
     return data;
 
   }
@@ -34,47 +37,36 @@ $(document).ready(function ($) {
 
 
 
+  var fetchArticleTitle = function (articleTitle) {
+    // console.log(articleTitle);
 
-  var fetchArticleTitle = function (data) {
-    console.log(data);
-    searchIfTextExist(data);
-
-    return data;
-  }
-
-  var fetchArticleTitleTranslated = function (data) {
-    console.log(data);
-
-    var translatedContent = function (data) {
-      return data;
+    if (searchIfTextExist(articleTitle)) {
+      findContainerElement(articleTitle);
     }
 
-
-    return data;
-  }
-
-
-  var fetchArticleContent = function (data) {
-    console.log(data);
-
-    // TODO Loop the array of data and call searchIfTextExist(data)
-
-
-    searchIfTextExist(data);
-
-    return data;
-  }
-
-  var fetchArticleContentTranslated = function (data) {
-    console.log(data);
-
-    // TODO Loop the array of data and call replaceText(data) ?
-
-    return data;
+    return articleTitle;
   }
 
 
 
+  var fetchArticleContent = function (articleContent) {
+    console.log(articleContent);
+
+    // TODO Loop the array of articleContent and call searchIfTextExist(data)
+
+
+    searchIfTextExist(articleContent);
+
+    return articleContent;
+  }
+
+  var fetchArticleContentTranslated = function (articleContentTranslated) {
+    console.log(articleContentTranslated);
+
+    // TODO Loop the array of articleContentTranslated and call replaceText(articleContentTranslated) ?
+
+    return articleContentTranslated;
+  }
 
 
 
@@ -131,25 +123,105 @@ $(document).ready(function ($) {
 
 
 
-  var searchIfTextExist = function (data) {
-
-    if (document.body.textContent.includes(data)) {
-      console.info("Text exist on page: " + data);
 
 
-      // call the appendText function then pass in data
-      appendText(data);
+
+  var searchIfTextExist = function (searchString) {
+
+    if (document.body.textContent.includes(searchString)) {
+      console.info("Text exist on page: " + searchString);
+
+      return true;
     } else {
-      console.error("Text does not exist on page: " + data );
+      console.error("Text does not exist on page: " + searchString);
+      return false;
+
     }
 
   }
 
 
-  var appendText = function (data) {
-    // TODO FIND THE PARENT where data is coming from and append it to the translatedContent
-    // data.parentNode.append("Lower Austria: Fake mails in the name of ÖVP members");
-    console.log(data.parentNode);
+  var findContainerElement = function (searchString) {
+    // Find the container element where data is coming from and pass the text
+
+    // The possible tags which can contain the text to be appended the newly translatedContent
+    const elements = [
+      "p",
+      // "div",
+      // "b"
+    ];
+    const matches = [];
+    var container;
+
+    for (let i = 0; i < elements.length; i++) {
+      var element = elements[i];
+
+      for (var element of document.querySelectorAll(element)) {
+        if (element.textContent.includes(searchString)) {
+          matches.push(element);
+        }
+      }
+      container = matches[matches.length - 1];
+
+      appendTitleTranslation(container, container.innerTex);
+    }
+
+
+    return (container, container.innerText);
+
+  }
+
+
+
+
+  var appendTitleTranslation = function (container) {
+
+
+
+
+    var fetchArticleTitleTranslated = function () {
+
+
+
+      var newblyBackendAPI = "https://api.newb.ly/articles/gVe8WHhm?language=english&country=austria&fbclid=IwAR3IA_dgK8W_kakCh44PUJv3lMajeJWYqIotGcSdlSMFnFRKGS3yeceZp3o";
+
+      $.ajax({
+        url: newblyBackendAPI,
+        dataType: "json",
+        success: function (result) {
+          var articleTitleTranslated = result.articleTitleTranslated;
+          var articleTitle = result.articleTitle;
+
+
+          console.log(articleTitleTranslated);
+          console.log(articleTitle);
+          console.log(container.innerText);
+          console.log(container);
+
+
+          if (container.innerText === articleTitle) {
+
+            // TODO Write actual logic to append text
+            console.log("We are going to append text here");
+
+            container.append("<div class='translated'>" + articleTitleTranslated + "</div>");
+          }
+
+
+        },
+        error: function (xhr) {
+          console.error("An error occurred: " + xhr.status + " " + xhr.statusText);
+        }
+      });
+    }
+
+
+    // console.log(container);
+    // console.log(container.innerText);
+
+
+
+    fetchArticleTitleTranslated();
   }
 
 
@@ -160,8 +232,57 @@ $(document).ready(function ($) {
 
 
 
-  // fetchArticleFromBackend();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  fetchArticleFromBackend();
 
 }(jQuery));
-
 
